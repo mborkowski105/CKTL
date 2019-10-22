63545fe65660c95d7d0a52f85ca35739255df825
Matthew <barnabytheplumber@gmail.com>
2019-10-22 15:40:46 -0400

    Added user/ingredient behavior, also seeds

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,6 +1,40 @@
+require 'pry'
+
 class User < ActiveRecord::Base
     has_many :user_cocktails
     has_many :cocktails, through: :user_cocktails
     has_many :user_ingredients
     has_many :ingredients, through: :user_ingredients
+
+    def inventory 
+        return UserIngredient.all.select do |ui|
+            ui.user_id == self.id
+        end
+    end
+
+    def add_inventory(new_name, new_quantity)
+        match = UserIngredient.find_by(user_id: self.id, ingredient_id: Ingredient.find_by(name: new_name).id)
+        
+        if !(match)
+            UserIngredient.create(user_id: self.id, ingredient_id: Ingredient.find_by(name: new_name).id, quantity: new_quantity)
+        else 
+            match.update(quantity: match.quantity + new_quantity)
+        end
+    end
+
+    def remove_inventory(new_name, new_quantity)
+        match = UserIngredient.find_by(user_id: self.id, ingredient_id: Ingredient.find_by(name: new_name).id)
+        
+        if (match.quantity <= new_quantity)
+            match.delete
+        else 
+            match.update(quantity: match.quantity - new_quantity)
+        end
+    end
+
+    def clear_inventory
+        self.inventory.each do |ui|
+            ui.delete
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
+Cocktail.delete_all
+Ingredient.delete_all
+CocktailIngredient.delete_all
+UserIngredient.delete_all
+
 Cocktail.create(name: "Straight Vod", directions: "Pour up")
+Cocktail.create(name: "Vodka and pedialyte", directions: "Full of healthy electrolytes")
+Cocktail.create(name: "Long island iced tea", directions: "Stop after one")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rum")
+CocktailIngredient.create(cocktail_id: Cocktail.find_by(name: "Straight Vod").id, ingredient_id: Ingredient.find_by(name: "vodka").id)
